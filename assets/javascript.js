caed28ddab28b4fdcf422b5308e66ad36e5539cc
NPH82 <nich1982@gmail.com>
2017-07-29 21:27:22 -0400

    got buttons added

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,40 +1,70 @@
-$(document).ready(function(){
+var farmAnimals = ["Pig", "Cow", "Chicken", "Horse", "Goat", "Sheep", "Turkey", "Rooster", "Duck", "Dog"];
 
-			$("#gif-submit").on("click", function(event){
-				event.preventDefault();
+$(document).ready(function() {
 
-			var searchTerm = $("#gif-entry").val();
+    
 
-			var apiKey = "e0bdabd1805e4798afae162920201534";
-			var protocol = "https://";
-			var domain = "api.giphy.com";
-			var path = "/v1/gifs/search";
-			var limit = 10;
-			
+    // $("#addAnimal").on("click", function(event) {
+    //     event.preventDefault();
 
 
-			var url = protocol + domain + path + "?" + "api_key=" + apiKey + "&q=" + searchTerm + "&limit=" + limit;
+    function showFarmAnimals(){
+        //var searchTerm = $("#farmAnimalEntry").val();
+        var searchTerm = $(this).attr("data-name");
 
-			$.ajax ({
-				url: url,
-				method: "GET"
-			}).done(function(response) {
+        var apiKey = "ae0b9e17b4d84cb8bdff9a172d556c2c";
+        var limit = 10;
 
-				displaySearch(response);
-			});
-		});
-
-		function displaySearch(response){
-			var returnedItems = (response).data;
-
-				for(var i=0; i < returnedItems.length; i++) {
-					var itemLimit = returnedItems[i].images.fixed_height_still.url;
-					var rating = returnedItems[i].rating;
-
-					$("#gif-list").append("<li><img src=" + itemLimit + "></img></li>");
-					$("#gif-list").append("<li>" + rating + "</li");
-				}
-			}
+        var url = "https://api.giphy.com/v1/gifs/search?" + "api_key=" + apiKey + "&q=" + searchTerm + "&limit=" + limit;
+        console.log(searchTerm);
 
 
-	});
+        $.ajax({
+            url: url,
+            method: "GET"
+        }).done(function(response) {
+
+            displaySearch(response);
+            createButton()
+        });
+    }
+
+       
+
+    function createButton() {
+        // $("#buttonArea").empty();
+        for (var i = 0; i < farmAnimals.length; i++) {
+            var createAnimal = $("<button>");
+            createAnimal.addClass("newAnimal");
+            createAnimal.attr("data-name", farmAnimals[i]);
+            createAnimal.text(farmAnimals[i]);
+            $("#buttonArea").append(createAnimal);
+        }
+    }
+
+
+    function displaySearch(response) {
+        var returnedItems = (response).data;
+
+        for (var i = 0; i < returnedItems.length; i++) {
+            var itemLimit = returnedItems[i].images.fixed_height_still.url;
+            var rating = returnedItems[i].rating;
+
+            $("#farmAnimallist").append("<li><img src=" + itemLimit + "></img></li>");
+            $("#farmAnimallist").append("<li>" + rating + "</li");
+        }
+    }
+
+     $("#addAnimal").on("click", function(event) {
+         event.preventDefault();
+
+        var anotherFarmAnimal = $("#farmAnimalEntry").val();
+        farmAnimals.push(anotherFarmAnimal);
+        console.log(farmAnimals);
+        createButton();
+     });
+     
+    $(document).on("click", ".newAnimal", showFarmAnimals);
+
+createButton();
+});
