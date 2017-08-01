880e37f17a4a43a6292441ab128ab3f696676f18
NPH82 <nich1982@gmail.com>
2017-07-31 21:42:41 -0400

    All Done

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,100 +1,98 @@
-var farmAnimals = ["Pig", "Cow", "Chicken", "Horse", "Goat", "Sheep", "Turkey", "Rooster", "Duck", "Dog"];
+var topics = ["Pig", "Cow", "Chicken", "Horse", "Goat", "Sheep", "Turkey", "Rooster", "Duck", "Dog"];
+
 $(document).ready(function() {
+       	// links the GET request 
+       	function displayFarmAnimal() {
+       		var farmAnimal = $(this).attr("data-name");
+       		 		apiKey = "ae0b9e17b4d84cb8bdff9a172d556c2c";
+       		 		limit = 10;
+       			 	URL = "https://api.giphy.com/v1/gifs/search?api_key=" + apiKey + "&q=" + farmAnimal + "&limit=" + limit;
+       	 	
+						$.ajax({
+       	 		url: URL,
+       			method: "GET"
+       	 	}).done(function(response) {
 
-    
-	// function showButtonAnimals(){
- //     $(".newAnimal").on("click", function(event) {
- //        //event.preventDefault();
+						var returnedItems = (response).data;
+						$("#farmAnimalOutput").empty();
 
- //        var farmAnimals = ["Pig", "Cow", "Chicken", "Horse", "Goat", "Sheep", "Turkey", "Rooster", "Duck", "Dog"];
- //        for (var i = 0; i < farmAnimals.length; i++)  {
- //        	//var farmAnimals = document.getElementsByClassName("newAnimal")[i].name;
- //        	if(this === farmAnimals) {
- //       			$("#farmAnimalEntry").val(this) ;
- //       			return farmAnimals[i];
- //        	}
- //       		console.log(document.getElementsByClassName("newAnimal")[i].name);
- //       		console.log(farmAnimals[i]);
- //    	}
- //    	console.log("#farmAnimalEntry");
- //        showFarmAnimals();
- //    	});
- //    };
+						//create div for images
+       			var animalDiv = $("<div class='farmImages'>");
 
+        		for (var i = 0; i < returnedItems.length; i++) {
+            var imgStill = returnedItems[i].images.fixed_height_still.url;
+            		imageMoving = returnedItems[i].images.fixed_height.url;
+            		// creating images
+            		image = $("<img class='gif' src='" + imgStill + "'>").attr({
+            				"data-still": imgStill,
+            				"data-animate": imageMoving,
+            				"data-state": "still",
+            			});
 
-    function showFarmAnimals(){
+            		animalDiv.append(image);
+        	     	rated = returnedItems[i].rating;
+        	     	paraRating = $("<h4>").text("Rating: " + rated);
+        	     	animalDiv.append(paraRating);
+       		 		 
+       		 		$("#farmAnimalOutput").append(animalDiv);
 
-        var searchAnimal= $("#farmAnimalEntry").val();
-        //var searchTerm = $("#buttonArea").attr("data-name");
+       		 	}
+       		 	pauseImage();
+       	 });
 
-        var apiKey = "ae0b9e17b4d84cb8bdff9a172d556c2c";
-        var limit = 10;
-
-        var url = "https://api.giphy.com/v1/gifs/search?" + "api_key=" + apiKey + "&q=" + searchAnimal + "&limit=" + limit;
-
-        
-        console.log(searchAnimal);
-
-
-        $.ajax({
-            url: url,
-            method: "GET"
-        }).done(function(response) {
-
-            displaySearch(response);
-            console.log("showFarmAnimals")
-           // createButton()
-        });
-    }
-
-       
-
-    function createButton() {
-         $("#buttonArea").empty();
-        for (var i = 0; i < farmAnimals.length; i++) {
-            var createAnimal = $("<button>");
-            createAnimal.addClass("newAnimal");
-            createAnimal.attr("name", farmAnimals[i]);
-            createAnimal.text(farmAnimals[i]);
-            $("#buttonArea").append(createAnimal);
-        }
-        console.log("createButton")
-    }
+       	}
 
 
-    function displaySearch(response) {
-        var returnedItems = (response).data;
 
-        $("#farmAnimallist").empty();
+       		//creates button out of array
+       	function createButtons() {
 
-        for (var i = 0; i < returnedItems.length; i++) {
-            var itemStill = returnedItems[i].images.fixed_height_still.url;
-            var itemMoving = returnedItems[i].images.fixed_height.url;
-            var rating = returnedItems[i].rating;
+       		$("#buttonArea").empty();
 
-            $("#farmAnimallist").append("<li><img src=" + itemStill + "></img></li>");
-            $("#farmAnimallist").append("<li><img src=" + itemMoving + "></img></li>");
-            $("#farmAnimallist").append("<li>" + rating + "</li");
-            console.log("displaysearch")
-        }
-    }
+       		for(var i=0; i < topics.length; i++){
+       			var btn = $("<button>");
+       			btn.addClass("farmAnimals");
+       			btn.attr("data-name", topics[i]);
+       			btn.text(topics[i]);
+       			$("#buttonArea").append(btn);
+       		};
+       	};
 
-     $("button").on("click", function(event) {
-         event.preventDefault();
+       		//adds new Farm Animals
+				$("#addAnimal").on("click", function(event){
+       	 	event.preventDefault();
+       	 	var anotherFarmAnimal = $("#farmAnimalEntry").val().trim();
+       	 	topics.push(anotherFarmAnimal);
+       	 	createButtons();
+       	 });
 
-        //$("#farmAnimallist").empty();
-        var	newFarmAnimal = $("#farmAnimalEntry").val();
-        farmAnimals.push(newFarmAnimal);
-        //console.log working here
-        console.log(farmAnimals);
-        createButton();
-        showFarmAnimals();
-        console.log("add Animal Button")
-     });
+				//toggles still to moving
+				function pauseImage() {
+					$(".gif").on("click", function(){
+						//event.preventDefault();
 
- 
+						var state = $(this).attr("data-state");
 
-    $(document).on("click", "#buttonArea", displaySearch);
+						
+						if(state == "still") {
+							$(this).attr("src", $(this).attr("data-animate"));
+							$(this).attr("data-state", "animate");
+							console.log(state)
+							return true;
+						} else {
+							$(this).attr("src", $(this).attr("data-still"));
+							$(this).attr("data-state", "still");
+							console.log("no it's this one")
+						}
 
-createButton();
+						
+					});
+
+				}
+
+
+
+				$(document).on("click", ".farmAnimals", displayFarmAnimal);
+				createButtons();
+
 });
